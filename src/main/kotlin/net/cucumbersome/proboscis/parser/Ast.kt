83f9bc1f2eed685368cf5a0240bd8279aeb0c304
Michal Gutowski <michal@cucumbersome.net>
2023-04-14 07:49:30 +0200

    Working parser

---
+++ src/main/kotlin/net/cucumbersome/proboscis/parser/Ast.kt
@@ -0,0 +1,20 @@
+package net.cucumbersome.proboscis.parser
+
+import net.cucumbersome.proboscis.Token
+
+sealed interface Node {
+    val token: Token
+}
+
+sealed interface Expression : Node
+
+class Identifier(val value: String, override val token: Token) : Expression
+
+class IntegerLiteral(val value: Int, override val token: Token) : Expression
+
+sealed interface Statement : Node
+
+class LetStatement(val name: Identifier, val value: Expression, override val token: Token) : Statement
+
+
+class Program(val statements: List<Statement>)
---
+++ src/test/kotlin/net/cucumbersome/proboscis/parser/TestParser.kt
@@ -0,0 +1,36 @@
+package net.cucumbersome.proboscis.parser
+import net.cucumbersome.proboscis.Token
+import net.cucumbersome.proboscis.lexer.Lexer
+import kotlin.test.Test
+import kotlin.test.assertEquals
+class TestParser {
+    @Test
+    fun testParsingLetStatements() {
+        val input = """
+            let x = 5;
+            let y = 10;
+            let foobar = 838383;
+        """.trimIndent()
+        val lexer = Lexer.fromString(input)!!
+        val parser = Parser(lexer)
+        val program = parser.parseProgram()
+        assertEquals(3, program.statements.size)
+        val tests = listOf(
+            "x" to 5,
+            "y" to 10,
+            "foobar" to 838383
+        )
+        tests.forEachIndexed { index, (expectedIdentifier, expectedValue) ->
+            val statement = program.statements[index]
+            if(statement !is LetStatement) {
+                throw AssertionError("Statement at index $index is not a LetStatement")
+            }
+            assertEquals(Token.Companion.Let, statement.token)
+            assertEquals(expectedIdentifier, statement.name.value)
+            if(statement.value !is IntegerLiteral) {
+                throw AssertionError("Value of statement at index $index is not an IntegerLiteral")
+            }
+            assertEquals(expectedValue, (statement.value as IntegerLiteral).value)
+        }
+    }
+}
